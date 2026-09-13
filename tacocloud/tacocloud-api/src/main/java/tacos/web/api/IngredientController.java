caf15012f110e97a78463889d65f07210e877745
DaiserNet <diegoadriel.s.r@gmail.com>
2026-09-13 16:36:33 -0600

    Solucion de retos TC-01 y TC-02

--- tacocloud/tacocloud-api/src/main/java/tacos/web/api/IngredientController.java
+++ tacocloud/tacocloud-api/src/main/java/tacos/web/api/IngredientController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,11 +45,19 @@ public class IngredientController {
   }
 
   @PutMapping("/{id}")
-  public void updateIngredient(@PathVariable String id, @RequestBody Ingredient ingredient) {
+  public Mono<ResponseEntity<Ingredient>> updateIngredient(@PathVariable String id, @RequestBody Ingredient ingredient) {
     if (!ingredient.getId().equals(id)) {
-      throw new IllegalStateException("Given ingredient's ID doesn't match the ID in the path.");
+      // throw new IllegalStateException("Given ingredient's ID doesn't match the ID in the path.");
+      return Mono.just(ResponseEntity.badRequest().build());
     }
-    repo.save(ingredient);
+
+    return repo.findById(id).flatMap(existingIngredient -> {
+      existingIngredient.setName(ingredient.getName());
+      existingIngredient.setType(ingredient.getType());
+      return repo.save(existingIngredient);
+    })
+        .map(updatedIngredient -> ResponseEntity.ok(updatedIngredient))
+        .defaultIfEmpty(ResponseEntity.notFound().build());
   }
 
   @PostMapping
@@ -63,8 +72,11 @@ public class IngredientController {
   }
 
   @DeleteMapping("/{id}")
-  public void deleteIngredient(@PathVariable String id) {
-    repo.deleteById(id);
+  public Mono<ResponseEntity<Ingredient>> deleteIngredient(@PathVariable String id) {
+    // repo.deleteById(id);\
+    return repo.findById(id).flatMap(existingIngredient -> repo.delete(existingIngredient)
+        .thenReturn(ResponseEntity.noContent().<Ingredient>build()))
+    .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
   }
 
 }
---
+++ tacocloud/tacocloud-api/src/test/java/tacos/web/api/IngredientControllerTest.java
@@ -0,0 +1,124 @@
+package tacos.web.api;
+
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+import reactor.test.publisher.PublisherProbe;
+import reactor.core.publisher.Mono;
+import tacos.Ingredient;
+import tacos.data.IngredientRepository;
+
+import org.junit.jupiter.api.Test;
+
+@ContextConfiguration(classes = IngredientController.class)
+@WebFluxTest(controllers = IngredientController.class, excludeAutoConfiguration = { org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration.class })
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IngredientControllerTest {
+    
+    @Autowired
+    private WebTestClient testClient;
+
+    @MockBean
+    private IngredientRepository repo; 
+
+    @Autowired
+    private IngredientController controller;
+
+    // Put test case
+    @Test
+    public void shouldUpdateIngredient() {
+        Mockito.when(repo.findById("FLTO"))
+            .thenReturn(Mono.just(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP)));
+        Mockito.when(repo.save(Mockito.any(Ingredient.class)))
+            .thenReturn(Mono.just(new Ingredient("FLTO", "New Flour Tortilla", Ingredient.Type.WRAP)));    
+        testClient.put().uri("/api/ingredients/FLTO")
+            .bodyValue(new Ingredient("FLTO", "New Flour Tortilla", Ingredient.Type.WRAP))
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody()
+                .jsonPath("$.id").isEqualTo("FLTO")
+                .jsonPath("$.name").isEqualTo("New Flour Tortilla")
+                .jsonPath("$.type").isEqualTo("WRAP");
+        
+        Mockito.verify(repo, Mockito.times(1)).findById("FLTO");
+        
+        Mockito.verify(repo, Mockito.times(1)).save(Mockito.argThat(ingredient -> 
+            ingredient.getId().equals("FLTO") &&
+            ingredient.getName().equals("New Flour Tortilla") &&
+            ingredient.getType() == Ingredient.Type.WRAP
+        ));
+        
+
+    }
+
+    @Test
+    public void shouldReturnBadRequestForMismatchedId() {
+        testClient.put().uri("/api/ingredients/FLTO")
+            .bodyValue(new Ingredient("LFTO", "Lettuce", Ingredient.Type.VEGGIES))
+            .exchange()
+            .expectStatus().isBadRequest();
+        Mockito.verify(repo, Mockito.times(0)).save(Mockito.any(Ingredient.class));
+    }
+
+    @Test
+    public void shouldReturnNotFoundForNonexistentIngredient() {
+        Mockito.when(repo.findById("LFTO")).thenReturn(Mono.empty());
+        testClient.put().uri("/api/ingredients/LFTO")
+            .bodyValue(new Ingredient("LFTO", "Lettuce", Ingredient.Type.VEGGIES))
+            .exchange()
+            .expectStatus().isNotFound();
+        Mockito.verify(repo, Mockito.times(0)).save(Mockito.any(Ingredient.class));
+    }
+
+    @Test
+    public void shouldEmitAndComplete() {
+        Ingredient ingredient = new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP);
+        
+        PublisherProbe<Ingredient> saveProbe = PublisherProbe.of(Mono.just(ingredient));
+        
+        Mockito.when(repo.findById("FLTO")).thenReturn(Mono.just(ingredient));
+        Mockito.when(repo.save(Mockito.any(Ingredient.class))).thenReturn(saveProbe.mono());
+
+        Mono<ResponseEntity<Ingredient>> responseMono = controller.updateIngredient("FLTO", ingredient);
+
+
+        StepVerifier.create(responseMono)
+            .expectNextMatches(response -> response.getStatusCode().is2xxSuccessful() && response.getBody().equals(ingredient))
+            .verifyComplete();
+
+        saveProbe.assertWasSubscribed();
+        saveProbe.assertWasRequested();
+        saveProbe.assertWasNotCancelled();
+    }
+
+    // Delete test case
+    @Test
+    public void shouldDeleteIngredient() {
+        Mockito.when(repo.findById("FLTO"))
+            .thenReturn(Mono.just(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP)));
+        Mockito.when(repo.delete(Mockito.any(Ingredient.class)))
+            .thenReturn(Mono.empty());
+        testClient.delete().uri("/api/ingredients/FLTO")
+            .exchange()
+            .expectStatus().isNoContent();
+        Mockito.verify(repo, Mockito.times(1)).delete(Mockito.any(Ingredient.class));
+        
+    }
+
+    @Test
+    public void shouldReturnNotFoundForDeleteNonexistentIngredient() {
+        Mockito.when(repo.findById("FLTO"))
+            .thenReturn(Mono.empty());
+        testClient.delete().uri("/api/ingredients/FLTO")
+            .exchange()
+            .expectStatus().isNotFound();
+        Mockito.verify(repo, Mockito.times(0)).delete(Mockito.any(Ingredient.class));
+    }
+    
+}
